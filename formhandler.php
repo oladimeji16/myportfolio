d99ce945521d56eddebfc57df170f980c539080a
oladimeji16 <olatohdimeji16@gmail.com>
2019-07-02 18:23:50 +0100

    made contact form with php\

---
+++ formhandler.php
@@ -0,0 +1,16 @@
+<?php
+    $name=$_POST['name'];
+    $visitor_email= $_POST['email'];
+    $message= $_POST['message']
+
+    $email_from= 'easytuts@gmail.com';
+    $email_subject= "New Submission"
+    $email_body="User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to="olatohdimeji16@gmail.com";
+    $headers="From: $email_from\r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header(location: "contact.html");
+?>
